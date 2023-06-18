f187157228140d3f52ea9391fc89874cc699ae03
Alpa <alpa@adminisatorsmbp.attlocal.net>
2023-06-17 22:09:08 -0700

    assignment1 completed

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ def main():
     # make your code below
     # print('Hello World')
     ##############################
+    print('Hello World')
+    print('Python Programming')
+    print("lolololol")
 
 
 if __name__ == '__main__':
